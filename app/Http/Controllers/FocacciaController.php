6580d6744a52148b3cbb36546ae26bc16f69f1fd
PatoRomano <patoromano22@gmail.com>
2022-05-11 22:55:02 -0300

    Versión Inicial
    Creación de proyecto laravel, base de datos configurada, modelo y controlador "Focaccia" creados.

---
+++ app/Http/Controllers/FocacciaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Focaccia;
+use Illuminate\Http\Request;
+
+class FocacciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Focaccia  $focaccia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Focaccia $focaccia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Focaccia  $focaccia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Focaccia $focaccia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Focaccia  $focaccia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Focaccia $focaccia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Focaccia  $focaccia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Focaccia $focaccia)
+    {
+        //
+    }
+}
